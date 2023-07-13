7427d95ceeddb571f3c2459a44bf4fcf7f23ce29
Divyansh Pandey <113619486+Divyansh0108@users.noreply.github.com>
2023-07-13 21:36:44 +0530

    Brand name generator introductory level project

---
+++ BandNameGeneratorProject.py
@@ -0,0 +1,14 @@
+print("Welcome to the Band-Name generator.")
+print("What is the name of the city you grew up in?\n")
+city = input()
+print("What is the name of your pet?\n")
+pet_name = input()
+
+print("Your band name could be "+city+pet_name+"\n")
+
+response = input("Did you like the generated brand-name? (Yes/No)\n")
+
+if response == "Yes":
+    print("Thank you for using the Band-Name generator!! ")
+else:
+    print("We will certainly improve our services!!")
